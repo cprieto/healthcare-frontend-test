b638476979055a4257ccb04f90e3eee8cf1f5a5b
Cristian Prieto <me@cprieto.com>
2018-07-10 17:42:34 +0200

    Redirect root to the swagger page

--- src/main/java/com/healthcare/sample/Application.java
+++ src/main/java/com/healthcare/sample/Application.java
@@ -3,11 +3,19 @@ package com.healthcare.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void addViewControllers (ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
     }
 }
