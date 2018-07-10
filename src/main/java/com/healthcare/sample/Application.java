7896a910782107bcfb35dccf53636416257cf367
Cristian Prieto <me@cprieto.com>
2018-07-10 17:54:52 +0200

    Remove configuration, it is not working

--- src/main/java/com/healthcare/sample/Application.java
+++ src/main/java/com/healthcare/sample/Application.java
@@ -3,19 +3,11 @@ package com.healthcare.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application extends WebMvcConfigurerAdapter {
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void addViewControllers (ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/swagger-ui.html");
     }
 }
